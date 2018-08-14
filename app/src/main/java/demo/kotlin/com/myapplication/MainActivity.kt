8121b55ba7ac64cf6d00d8e4a8fe7bc12dd5b559
DIM <linhuorong@linhuorongdeMacBook-Pro.local>
2018-08-15 00:41:42 +0800

    add some func

--- app/src/main/java/demo/kotlin/com/myapplication/MainActivity.kt
+++ app/src/main/java/demo/kotlin/com/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package demo.kotlin.com.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         message.text="test kotlin"
         var musician:Artist
+        message.text="sub "+sub(1,3)
+
+        toast("test toast fun")
 
     }
 
@@ -18,4 +22,15 @@ class MainActivity : AppCompatActivity() {
                       var name: String,
                       var url:  String,
                       var age: Int)
+
+    fun add(x:Int,y:Int):Int=x+y
+
+    fun sub(x:Int,y:Int):Int=x-y
+
+
+    fun toast(str:String,time:Int= Toast.LENGTH_LONG)
+    {
+        Toast.makeText(this,str,time)
+    }
+
 }
